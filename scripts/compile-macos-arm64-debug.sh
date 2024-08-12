d8fb44c01bae46dda82032dfb9f3d28114229b8b
Radu Salagean <radusalagean@hotmail.com>
2024-08-12 20:45:03 +0000

    Adjust mac linking order

--- scripts/compile-macos-arm64-debug.sh
+++ scripts/compile-macos-arm64-debug.sh
@@ -7,8 +7,8 @@ cd $PROJECT_ROOT_DIR
 
 oa64-clang++ --std=c++17 \
     -I/SFML/include \
+    src/main.cpp src/platform/unix/macos/MacOSImpl.cpp \
     -L/SFML/build/lib \
     -lsfml-graphics-d -lsfml-window-d -lsfml-system-d -lsfml-audio-d \
     -Wl,-rpath,'@executable_path/../lib' \
-    -o build/macos-arm64-debug/bin/sfml-test \
-    src/main.cpp src/platform/unix/macos/MacOSImpl.cpp
+    -o build/macos-arm64-debug/bin/sfml-test
